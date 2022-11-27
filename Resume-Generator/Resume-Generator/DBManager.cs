f4cde0f38df0d6775c97541ed895ac287b40ff63
Elizabeth Maspoch <musicengineerucf@gmail.com>
2022-11-27 16:49:47 -0500

    db read, write, create missing: restructure and delete

---
+++ Resume-Generator/Resume-Generator/DBManager.cs
@@ -0,0 +1,155 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resume_Generator
+{
+    public class DBManager {
+        private string DBPath;
+
+        public DBManager()
+        {
+            DBPath = getDBDirectory();
+        }
+
+        private string generateGUID() { 
+            return Guid.NewGuid().ToString();
+        }
+
+        public BasicUser createNewUser(string FrstName, string mdlName, string LstName) {
+            string filename = FrstName + "+" + LstName + "=" + generateGUID();
+            if (mdlName != "") { 
+                filename = FrstName + "+" + mdlName + "+" + LstName + "=" + generateGUID(); 
+            }
+
+            string newfilePath = DBPath + "\\" + filename + ".json";
+            System.IO.File.Copy(DBPath+"\\Default.json", newfilePath);
+            Dictionary<string, Dictionary<string, string>> UserDict = LoadJson(newfilePath);
+            UserDict["Profile"]["firstname"] = FrstName;
+            UserDict["Profile"]["lastname"] = LstName;
+            UserDict["Profile"]["middlename"] = mdlName;
+            updateJson(UserDict, newfilePath);
+            return new BasicUser(newfilePath, DBPath);
+
+        }
+
+        private void updateJson(Dictionary<string, Dictionary<string, string>> myDictionary, string filename) {
+            var updatedDict = JsonConvert.SerializeObject(myDictionary);
+            File.WriteAllText(filename, updatedDict);
+        }
+
+        public Dictionary<string, Dictionary<string, string>> LoadJson(string myFileName)
+        {
+            Dictionary<string, Dictionary<string, string>> mydictionary = new Dictionary<string, Dictionary<string, string>>();
+            var text = File.ReadAllText(myFileName);
+            mydictionary = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(text);
+            return mydictionary;
+        }
+
+        public List<BasicUser> getAllUsers()
+        {
+            BasicUser[] Users;
+            List<BasicUser> myCollection = new List<BasicUser>();
+            var files = Directory.GetFiles(DBPath, "*.*", SearchOption.AllDirectories).Where(name => !name.Contains("Default")); ;
+            foreach (string file in files)
+            {
+                myCollection.Add(new BasicUser(file, DBPath));   
+            };
+            return myCollection;
+        }
+
+        private string getDBDirectory()
+        {
+            string DBPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            int index = DBPath.LastIndexOf("bin");
+            if (index >= 0)
+                DBPath = DBPath.Substring(0, index); // or index + 1 to keep slash
+            DBPath = DBPath + "Resources\\DB";
+            return DBPath;
+        }
+
+
+    };
+
+    public class BasicUser {
+
+        public BasicUser(string filename, string dbpath) {
+            FileName = filename;
+            DBPath = dbpath;
+            GUID = GetGUIDFromName(filename);
+            FirstName = GetFirstNameFromName(filename);
+            LastName = GetLastNameFromName(filename);
+            MiddleName = GetMiddleNameFromName(filename);
+
+        }
+        public string FirstName = "John";
+        public string LastName = "Jingleheimerschmit";
+        public string MiddleName = "Jacob";
+        public string GUID;
+        public string FileName;
+        string DBPath;
+
+        private string GetGUIDFromName(string filename)
+        {
+            filename = filename.Substring(filename.Length - 41);
+            filename = filename.Substring(0, filename.Length - 5);
+            return filename;
+        }
+
+        private string GetFirstNameFromName(string filename)
+        {
+            filename = filename.Remove(0, DBPath.Length + 1);
+            filename = filename.Replace("=", " ");
+            if (filename.Length >= 48) {
+                filename = filename.Substring(0, filename.Length - 5);
+                filename = filename.Substring(0, filename.Length - 41);
+            }
+            int index = filename.IndexOf("+");
+            if (index >= 0)
+                filename = filename.Substring(0, index);
+            return filename;
+        }
+
+        private string GetLastNameFromName(string filename)
+        {
+            if (filename.Length >= 43)
+            {
+                filename = filename.Remove(0, DBPath.Length + 1);
+                filename = filename.Replace("=", " ");
+                filename = filename.Substring(0, filename.Length - 41);
+            }
+            int index = filename.LastIndexOf("+");
+            if (index >= 0)
+            {
+                filename = filename.Substring(index + 1);
+            }
+            return filename;
+        }
+
+        private string GetMiddleNameFromName(string filename)
+        {
+            if (filename.Length >= 43)
+            {
+                filename = filename.Remove(0, DBPath.Length + 1);
+                filename = filename.Replace("=", " ");
+                filename = filename.Substring(0, filename.Length - 41);
+            }
+            int frstindex = filename.IndexOf("+");
+            int lstindex = filename.LastIndexOf("+");
+            if (frstindex == lstindex)
+            {
+                filename = "";
+            }
+            else {
+                if (frstindex >= 0 && lstindex >= 0) {
+                    filename.Substring(frstindex, lstindex);
+                }
+            }
+            return filename;
+        }
+    }
+}
--- Resume-Generator/Resume-Generator/Startup.xaml.cs
+++ Resume-Generator/Resume-Generator/Startup.xaml.cs
@@ -1,14 +1,173 @@
+using Newtonsoft.Json;
+using SkiaSharp;
+using System.Runtime.Versioning;
+using System.Windows;
+
 namespace Resume_Generator;
 
 public partial class Startup : ContentPage
 {
-	public Startup()
-	{
-		InitializeComponent();
-	}
+    string firstName = "";
+    string middleName = "";
+    string lastName = "";
+    DBManager db;
+    List<BasicUser> Users;
+    public Startup()
+    {
+        InitializeComponent();
+        db = new DBManager();
+        Users = db.getAllUsers();
+        foreach (var user in Users.ToArray()) {
+            createRow(user);
+        }
+            
+        
+    }
+    private HorizontalStackLayout createRow(BasicUser user) {
+        HorizontalStackLayout row = new HorizontalStackLayout();
+        
+        Label lbl1 = new Label();
+        Label lbl2 = new Label();
+        Label lbl3 = new Label();
+        CheckBox chckbx = new CheckBox();
+        chckbx.CheckedChanged += new EventHandler<CheckedChangedEventArgs>(CheckBox_Changed);
+        
+        lbl1.Text = user.FirstName;
+        lbl1.Margin += 5;
+        lbl1.Margin += 5;
+        lbl2.Text = user.MiddleName;
+        lbl2.Margin += 5;
+        lbl2.Margin += 5;
+        lbl3.Text = user.LastName;
+        lbl3.Margin += 5;
+        lbl3.Margin += 5;
 
-	private void ConfirmBtn_Clicked(object sender, EventArgs e)
-	{
-		Navigation.PushAsync(new MainPage());
-	}
+        row.Children.Add(chckbx);
+        row.Children.Add(lbl1);
+        row.Children.Add(lbl2);
+        row.Children.Add(lbl3);
+        UserList.Children.Add(row);
+        return row;
+    }
+
+    private async void ConfirmBtn_Clicked(object sender, EventArgs e)
+    {
+        string action = "Retry";
+        string result;
+        do
+        {
+            result = await DisplayPromptAsync("Decryption", "Password:");
+            if (result == "password")
+            {
+                await DisplayAlert("Success!", "file successfully decrypted", "OK");
+                Navigation.PushAsync(new MainPage());
+                action = "Success";
+            }
+            else if (result == null)
+            {
+                action = "canceled";
+            }
+            else
+            {
+                action = await DisplayActionSheet("Incorrect password", "Retry", "Give Up");
+            }
+
+        } while (action == "Retry");
+
+    }
+
+    private async void DeleteBtn_Clicked(object sender, EventArgs e)
+    {
+
+
+    }
+
+    private void CheckBox_Changed(object sender, EventArgs e)
+    {
+
+
+    }
+
+    private async void NewBtn_Clicked(object sender, EventArgs e)
+    {
+        await requestFirstName(sender, e);
+        BasicUser newUser = db.createNewUser(firstName, middleName, lastName);
+        Users.Add(newUser);
+        createRow(newUser);
+
+    }
+
+    private async Task requestFirstName(object sender, EventArgs e) {
+        string action = "Retry";
+        do
+        {
+            firstName = await DisplayPromptAsync("User Creation", "First Name:");
+            if (firstName == "")
+            {
+                await DisplayAlert("User Creation", "First Name Required", "OK");
+                action = "Retry";
+            }
+            else if (firstName == null)
+            {
+                action = "canceled";
+            }
+            else
+            {
+                await requestMiddleName(sender, e);
+                action = "completed";
+            }
+
+        } while (action == "Retry");
+    }
+
+    private async Task requestMiddleName(object sender, EventArgs e)
+    {
+        string action = "Resubmit";
+        do
+        {
+            middleName = await DisplayPromptAsync("User Creation", "Middle Name:");
+            if (middleName == "")
+            {
+                action = await DisplayActionSheet("No Middle Name Entered", "Resubmit", "Continue");
+                if (action == "Continue") {
+                    await requestLastName(sender, e);
+                    action = "completed";
+                }
+            }
+            else if (middleName == null)
+            {
+                action = "canceled";
+            }
+            else
+            {
+                await requestLastName(sender, e);
+                action = "completed";
+            }
+
+        } while (action == "Resubmit");
+    }
+
+    private async Task requestLastName(object sender, EventArgs e)
+    {
+        string action = "Retry";
+        do
+        {
+            lastName = await DisplayPromptAsync("User Creation", "Last Name:");
+            if (lastName == "")
+            {
+                await DisplayAlert("User Creation", "Last Name Required", "OK");
+                action = "Retry";
+            }
+            else if (lastName == null)
+            {
+                action = "canceled";
+            }
+            else
+            {
+                action = "completed";
+            }
+
+        } while (action == "Retry");
+    }
+
 }
